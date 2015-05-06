b8365ac76f4702431a27bd96fa419d2ebe4380b1
Matt Stine <mstine@gopivotal.com>
2015-05-06 15:09:42 -0400

    upgrade to PactJVM 2.1.13, port to @PactRule testing model

--- microservices-pact-consumer/src/test/java/io/pivotal/microservices/pact/consumer/ConsumerPortTest.java
+++ microservices-pact-consumer/src/test/java/io/pivotal/microservices/pact/consumer/ConsumerPortTest.java
@@ -1,8 +1,9 @@
 package io.pivotal.microservices.pact.consumer;
 
-import au.com.dius.pact.consumer.ConsumerPactBuilder;
-import au.com.dius.pact.consumer.ConsumerPactTest;
+import au.com.dius.pact.consumer.*;
 import au.com.dius.pact.model.PactFragment;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -10,9 +11,13 @@ import java.util.Map;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class ConsumerPortTest extends ConsumerPactTest {
-    @Override
-    protected PactFragment createFragment(ConsumerPactBuilder.PactDslWithProvider builder) {
+public class ConsumerPortTest {
+
+    @Rule
+    public PactRule rule = new PactRule("localhost", 8080, this);
+
+    @Pact(state="Foo_State", provider="Foo_Provider", consumer="Foo_Consumer")
+    public PactFragment createFragment(ConsumerPactBuilder.PactDslWithProvider.PactDslWithState builder) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json;charset=UTF-8");
 
@@ -26,18 +31,9 @@ public class ConsumerPortTest extends ConsumerPactTest {
                 .body("[{\"value\":42}, {\"value\":100}]").toFragment();
     }
 
-    @Override
-    protected String providerName() {
-        return "Foo_Provider";
-    }
-
-    @Override
-    protected String consumerName() {
-        return "Foo_Consumer";
-    }
-
-    @Override
-    protected void runTest(String url) {
-        assertEquals(new ConsumerPort(url).foos(), Arrays.asList(new Foo(42), new Foo(100)));
+    @Test
+    @PactVerification("Foo_State")
+    public void runTest() {
+        assertEquals(new ConsumerPort("http://localhost:8080").foos(), Arrays.asList(new Foo(42), new Foo(100)));
     }
 }
