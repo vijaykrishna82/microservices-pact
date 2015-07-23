1e46e0ecf72712ae0e7667b2b59546f2cf680b1f
Matt Stine <mstine@gopivotal.com>
2015-07-22 21:37:15 -0600

    add basic endpoint to consume foos from provider and return JSON response

--- microservices-pact-consumer/src/main/java/io/pivotal/microservices/pact/consumer/ConsumerPort.java
+++ microservices-pact-consumer/src/main/java/io/pivotal/microservices/pact/consumer/ConsumerPort.java
@@ -1,17 +1,22 @@
 package io.pivotal.microservices.pact.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@Component
 public class ConsumerPort {
 
     private String url;
     private RestTemplate restTemplate;
 
-    public ConsumerPort(String url) {
+    @Autowired
+    public ConsumerPort(@Value("${producer}") String url) {
         this.url = url;
         this.restTemplate = new RestTemplate();
     }
